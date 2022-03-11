8b04d4430e9a69060443ea7043ebd03a00aae9dd
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-12 03:04:07 +0700

    fix: callback url broken

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
+import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
 
 /**
@@ -20,10 +21,18 @@ export default function HomePage() {
   const { data: session } = useSession();
   if (session) {
     return (
-      <>
-        Signed in as {session?.user?.email} <br />
+      <section className='mx-auto flex flex-col items-center justify-center'>
+        <p>Signed in as - {session?.user?.name} </p>
+        <p>Email - {session?.user?.email} </p>
+        <NextImage
+          src={session?.user?.image as string}
+          alt='Profile'
+          layout='fixed'
+          width={200}
+          height={200}
+        />
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </section>
     );
   }
   return (
@@ -35,7 +44,7 @@ export default function HomePage() {
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
             Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
+            <button onClick={() => signIn('google')}>Sign in</button>
           </div>
         </section>
       </main>
