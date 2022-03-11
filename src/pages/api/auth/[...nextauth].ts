468901becfa505d9ca0cdd0384c78907d3439480
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-11 17:47:45 +0700

    feat: migrate auth model

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
