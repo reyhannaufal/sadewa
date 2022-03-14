e00350af3a789e1941b75cd0bc7c8723a308bae7
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-14 13:46:48 +0700

    feat: update `seo`

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,12 +5,12 @@ import { openGraph } from '@/lib/helper';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Sadewa',
+  siteName: 'Sadewa',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Sadewa is fullstack NextJS app built with NextJS, TypeScript, and TailwindCSS.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://sadewa.vercel.app',
   type: 'website',
   robots: 'follow, index',
   /** No need to be filled, will be populated with openGraph function */
