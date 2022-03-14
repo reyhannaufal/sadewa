a48a066f5555b121e13bda6adbada912862d36fb
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-14 10:04:17 +0700

    feat: handle production auth token

--- src/pages/dashboard/_middleware.ts
+++ src/pages/dashboard/_middleware.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { isDevelopment } from '@/utils/env';
+
 export default function middleware(req: NextRequest) {
-  const authToken = req.cookies['next-auth.session-token'];
+  const authToken =
+    req.cookies[
+      isDevelopment()
+        ? 'next-auth.session-token'
+        : '__Secure-next-auth.session-token'
+    ];
   if (!authToken) {
     return NextResponse.redirect(new URL('/', req.url));
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { NextPageContext } from 'next';
-import { getSession, signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import * as React from 'react';
 
 import Button from '@/components/buttons/Button';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+
+import { isDevelopment } from '@/utils/env';
 
 interface HomeContext extends NextPageContext {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,7 +14,12 @@ interface HomeContext extends NextPageContext {
 }
 
 export async function getServerSideProps(context: HomeContext) {
-  const token = context.req?.cookies['next-auth.session-token'];
+  const token =
+    context.req?.cookies[
+      isDevelopment()
+        ? 'next-auth.session-token'
+        : '__Secure-next-auth.session-token'
+    ];
   if (token) {
     return {
       redirect: {
@@ -21,15 +28,11 @@ export async function getServerSideProps(context: HomeContext) {
       },
     };
   }
-
-  return {
-    props: {
-      session: await getSession(context),
-    },
-  };
+  return {};
 }
 
 export default function HomePage() {
+  const { status } = useSession();
   return (
     <Layout>
       <Seo templateTitle='Home' />
@@ -37,7 +40,12 @@ export default function HomePage() {
       <main>
         <div className='layout flex min-h-screen w-full flex-col items-center justify-center space-y-4 text-center'>
           <h3>Please signed in ❌</h3>
-          <Button onClick={() => signIn('google')}>Sign in</Button>
+          <Button
+            isLoading={status === 'loading'}
+            onClick={() => signIn('google')}
+          >
+            Sign in
+          </Button>
         </div>
       </main>
     </Layout>
