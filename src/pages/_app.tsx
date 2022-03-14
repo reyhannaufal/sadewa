632d676407f6a54b255b076059e44ae16878404f
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-14 23:43:54 +0700

    feat: add loading state between route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
 import { SessionProvider } from 'next-auth/react';
+import NProgress from 'nprogress';
 
+import 'nprogress/nprogress.css';
 import '@/styles/globals.css';
+
+NProgress.configure({
+  minimum: 0.8,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: true,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
